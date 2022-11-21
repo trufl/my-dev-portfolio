ca39fdd16cc32754c00344c5e033b69e193e9eee
T <endlesstruffle80@gmail.com>
2022-11-21 03:35:35 -0700

    adds styling

--- src/components/Projects/SideProjects/SideProjects.js
+++ src/components/Projects/SideProjects/SideProjects.js
@@ -1,4 +1,7 @@
-import { Card, Grid, Text} from '@nextui-org/react';
+import { Card, Grid, Text, Row, Link, Button } from '@nextui-org/react';
+import cliLogo from '../../../images/cli-logo.png';
+import quiz from '../../../images/quiz.png';
+import weather from '../../../images/weather.png';
 
 function SideProjects () {
     return (
@@ -11,51 +14,157 @@ function SideProjects () {
             <Grid.Container gap={2} justify='center'>
                 
                 <Grid xs={16} md={3}>
-                    <Card isHoverable isPressable>
-                        <Card.Header>
-                            <Text h3>
+                    <Card isHoverable >
+                        <Card.Header css={{ position: "absolute", zIndex: 1, top: -10 }}>
+                            <Text h3 color='warning' >
                                 JavaScript Quiz
                             </Text>
                         </Card.Header>
-                        <Card.Divider/>
-                        <Card.Body>
-                            <Text>A small quiz on JavaScript topics</Text>
-                            <Text>
+                        <Card.Body css={{ position: "absolute", zIndex: 1, top: 11}}>
+                            <Text b color='warning' >A small quiz on JavaScript topics</Text>
+                            <Text b color='warning' >
                                 HTML, JavaScript, CSS
                             </Text>
                         </Card.Body>
+                        <Card.Image
+                            src={quiz}
+                            objectFit='cover'
+                            alt='Quiz App Homepage'
+                            width="100%"
+                            height={340}
+                            />
+                            <Card.Footer
+                                isBlurred
+                                css={{
+                                    position: "absolute",
+                                    bgBlur: "#0f111466",
+                                    borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                                    bottom: 0,
+                                    zIndex: 1,
+                                }}
+                            >
+                                <Row justify="center">
+                                    <Button  flat shadow auto rounded color="primary">
+                                        <Link 
+                                            color="primary"
+                                            target="_blank"
+                                            href="https://trufl.github.io/javascript-quiz/"
+                                        >
+                                            <Text
+                                                css={{ color: "inherit" }}
+                                                size={12}
+                                                weight="bold"
+                                                transform="uppercase"
+                                            >
+                                                Visit App
+                                            </Text>
+                                        </Link>
+                                    </Button>
+                                </Row>
+                            </Card.Footer>
                     </Card>
                 </Grid>
                 <Grid xs={16} md={3}>
-                    <Card isHoverable isPressable>
-                        <Card.Header>
-                            <Text h3>
+                    <Card isHoverable >
+                        <Card.Header css={{ position: "absolute", zIndex: 1, top: -10 }}>
+                            <Text h3 color='warning' >
                                 Weather Application
                             </Text>
                         </Card.Header>
                         <Card.Divider/>
-                        <Card.Body>
-                            <Text>Search for a city's current day weather and a five day forecast</Text>
-                            <Text>
+                        <Card.Body css={{ position: "absolute", zIndex: 1, top: 11}}>
+                            <Text b color='warning' >Current weather and a five day forecast</Text>
+                            <Text b color='warning' >
                                 JavaScript, APIs
                             </Text>
                         </Card.Body>
+                        <Card.Image
+                            src={weather}
+                            objectFit='cover'
+                            alt='Weather App Homepage'
+                            width="100%"
+                            height={340}
+                            />
+                            <Card.Footer
+                                isBlurred
+                                css={{
+                                    position: "absolute",
+                                    bgBlur: "#0f111466",
+                                    borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                                    bottom: 0,
+                                    zIndex: 1,
+                                }}
+                            >
+                                <Row justify="center">
+                                    <Button  flat shadow auto rounded color="primary">
+                                        <Link 
+                                            color="primary"
+                                            target="_blank"
+                                            href="https://trufl.github.io/weather-application/"
+                                        >
+                                            <Text
+                                                css={{ color: "inherit" }}
+                                                size={12}
+                                                weight="bold"
+                                                transform="uppercase"
+                                            >
+                                                Visit App
+                                            </Text>
+                                        </Link>
+                                    </Button>
+                                </Row>
+                            </Card.Footer>
                     </Card>
                 </Grid>
                 <Grid xs={16} md={3}>
-                    <Card isHoverable isPressable>
-                        <Card.Header>
-                            <Text h3>
+                    <Card isHoverable >
+                        <Card.Header css={{ position: "absolute", zIndex: 1, top: 0, left: 70}}>
+                            <Text h3 color='warning' >
                                 Employee Tracker
                             </Text>
                         </Card.Header>
-                        <Card.Divider/>
-                        <Card.Body>
-                            <Text>A back-end CLI application</Text>
-                            <Text>
+                        <Card.Body css={{ position: "absolute", zIndex: 1, top: 120, left: 40 }}>
+                            <Text b color='warning' >A back-end CLI application</Text>
+                            <Text b color='warning' >
                                 JavaScript, Sequelize, Inquirer
                             </Text>
                         </Card.Body>
+                        <Card.Image
+                            src={cliLogo}
+                            objectFit='cover'
+                            alt='Command Line Interface Logo'
+                            width="100%"
+                            height={340}
+                            />
+                            <Card.Footer
+                                isBlurred
+                                css={{
+                                    position: "absolute",
+                                    bgBlur: "#0f111466",
+                                    borderTop: "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                                    bottom: 0,
+                                    zIndex: 1,
+                                }}
+                            >
+                                <Row justify="center">
+                                    <Button  flat shadow auto rounded color="primary">
+                                        <Link 
+                                            color="primary"
+                                            target="_blank"
+                                            href="https://github.com/trufl/Employee-Tracker-Application"
+                                        >
+                                            <Text
+                                                css={{ color: "inherit" }}
+                                                size={12}
+                                                weight="bold"
+                                                transform="uppercase"
+                                            >
+                                                Visit App
+                                            </Text>
+                                        </Link>
+                                    </Button>
+                                </Row>
+                            </Card.Footer>
                     </Card>
                 </Grid>
             </Grid.Container>
